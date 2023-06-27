c3a7fef927fe2b92bd89b01305773234e78c90b4
조경은 <kavvy99@naver.com>
2023-06-27 17:23:56 +0900

    string

---
+++ practice.py/numeric.py
@@ -0,0 +1,10 @@
+station = ['사당','신도림','인천공항']
+print(station[0]+'행 열차가 들어오고 있습니다.')
+
+
+print(abs(-5)) # 5
+print(pow(4, 2)) # 4^2 = 16
+print(max(5, 12)) # 최댓값 12
+print(min(5, 12)) # 최솟값 5
+print(round(3.14)) # 반올림 3
+print(round(4.98)) # 5
---
+++ str.py
@@ -0,0 +1,9 @@
+sentence = 'I am a girl'
+print(sentence)
+sentence2 = "python is easy"
+print(sentence2)
+sentence3 = """
+I am a girl.
+python is easy
+"""
+print(sentence3)
