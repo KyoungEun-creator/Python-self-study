b3c3398f08683a442f999a380139ebeb5a2ae10c
조경은 <kavvy99@naver.com>
2023-06-27 16:48:52 +0900

    first commit

--- practice.py
+++ practice.py
@@ -7,9 +7,9 @@ print(pow(4, 2)) # 4^2 = 16
 print(max(5, 12)) # 최댓값 12
 print(min(5, 12)) # 최솟값 5
 print(round(3.14)) # 반올림 3
-print(round(4.99)) # 5
+print(round(4.98)) # 5
 
 from math import *
-print(floor(4.99)) # 내림 4
+print(floor(4.98)) # 내림 4
 print(ceil(3.14)) # 올림 4
 print(sqrt(16)) # 제곱근 4
