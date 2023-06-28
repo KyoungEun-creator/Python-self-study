02c571bf8b39551aef84e3a22d01bad90129b7b1
조경은 <kavvy99@naver.com>
2023-06-28 17:07:47 +0900

    escape Characters

---
+++ practice.py/escapeCharacters.py
@@ -0,0 +1,22 @@
+# \n : 문장 내 줄바꿈
+
+print("백문이 불여일견\n백견이 불여일타")
+
+# 저는 "조경은"입나다.
+print("저는 '조경은'입니다.")
+print('저는 "조경은"입니다.')
+print("저는 \"조경은\"입니다.")
+print("저는 \'조경은\'입니다.")
+
+
+# \\ : 문장 내 \
+print("i don't like apple\\.") # i don't like apple\.
+
+#\r : 커서를 맨 앞 
+print("red apple\rPine") # Pineapple
+
+#\b : backspace 한 글자 삭제
+print("redd\b apple") # red apple
+
+#\t : tab
+print("Red\tApple") #Red    Apple
