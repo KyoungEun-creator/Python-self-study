c6bd0d2573e7181dd2fde17a6eb4472d1ff7e61e
조경은 <kavvy99@naver.com>
2023-06-29 18:37:24 +0900

    list

--- practice.py/5-1list.py
+++ practice.py/5-1list.py
@@ -10,35 +10,35 @@ subway2 = 20
 subway3 = 30
 
 subway = [10,20,30]
-print(subway) 
+print(subway)               # [10, 20, 30]
 
 subway = ["a","b","c"]
-print(subway) 
+print(subway)               # ['a', 'b', 'c']
 
 # "a"는 어디에 있는가
-print(subway.index("a"))
+print(subway.index("a"))    # 0
 
 # "d"를 맨 마지막에 추가
-subway.append("d")
-print(subway)
+subway.append("d")          
+print(subway)               # ['a', 'b', 'c', 'd]
 
 # "e"를 "a" "b" 사이에 추가
 subway.insert(1,"e")
-print(subway)
+print(subway)               # ['a', 'e', 'b', 'c', 'd']
 
 # 맨 마지막에서부터 하나씩 꺼냄
-print(subway.pop())     # 맨 마지막 것 출력
-print(subway)           # 맨 마지막 것을 제외한 나머지 출력
+print(subway.pop())         # 맨 마지막 것 출력 d
+print(subway)               # 맨 마지막 것을 제외한 나머지 출력 ['a', 'e', 'b', 'c']
 
 # 같은 것이 몇 개 있는지 확인
 subway.append("a")
-print(subway)
-print(subway.count("a"))
+print(subway)               # ['a', 'e', 'b', 'c', 'a']
+print(subway.count("a"))    # 2
 
 # 정렬하기
 num_list = [5,2,4,3,1]
 num_list.sort()
-print(num_list)         # [1, 2, 3, 4, 5]
+print(num_list)             # [1, 2, 3, 4, 5]
 
 # 순서 뒤집기
 num_list.reverse()      
